28379829e24bcfd7eae76fd4876529d4806752d5
MattAUT <110206916+MattAUT@users.noreply.github.com>
2022-07-29 13:37:00 +1200

    create app framework

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const articles = require("./dummydata/articles");
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("API is Running");
+});
+
+server.get("/api/articles", (req, res) => {
+  const article = articles.find((n) => n._id === req.params.id);
+  res.json(article);
+});
+
+server.listen(PORT, console.log(`Server is running on port ${PORT}`));

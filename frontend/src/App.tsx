55c84afe928363f202db1151a67a65b43e38a265
MattAUT <110206916+MattAUT@users.noreply.github.com>
2022-07-29 13:41:43 +1200

    Add increment counter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <h1>Welcome to CISE - the home of learning and fun</h1>
+    <div>
+      <p>Count: {count}</p>
+      <button className="increment" onClick={increment}>
+        Increment count
+      </button>
     </div>
   );
 };
